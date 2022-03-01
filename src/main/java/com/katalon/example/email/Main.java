6eb14d54145f54c100b82c6f37eeb14741ebacf5
Duy Luong <duyluong@kms-technology.com>
2022-03-01 10:43:22 +0700

    Update

--- src/main/java/com/katalon/example/email/Main.java
+++ src/main/java/com/katalon/example/email/Main.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 
 public class Main {
 
-    public static void main(String [] args) {
+    public static void main(String [] args) throws Exception {
 
         // Recipient's email ID needs to be mentioned.
         String to = System.getProperty("email.to");
@@ -36,27 +36,23 @@ public class Main {
             }
         });
 
-        try {
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
+        // Create a default MimeMessage object.
+        MimeMessage message = new MimeMessage(session);
 
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
+        // Set From: header field of the header.
+        message.setFrom(new InternetAddress(from));
 
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        // Set To: header field of the header.
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
-            // Set Subject: header field
-            message.setSubject("Katalon sample email test!");
+        // Set Subject: header field
+        message.setSubject("Katalon sample email test!");
 
-            // Now set the actual message
-            message.setText("This is actual message");
+        // Now set the actual message
+        message.setText("This is actual message");
 
-            // Send message
-            Transport.send(message);
-            System.out.println("Sent message successfully....");
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
-        }
+        // Send message
+        Transport.send(message);
+        System.out.println("Sent message successfully....");
     }
 }
